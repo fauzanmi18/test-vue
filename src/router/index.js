b32b5023dc47452e9827933a6d828f8dbb0d2e7a
fauzanmi18 <iqbal.darussalam17@gmail.com>
2024-07-19 20:49:14 +0700

    check history

--- src/router/index.js
+++ src/router/index.js
@@ -132,8 +132,9 @@ const routes = [
   },
 ]
 
+console.log(`ie history`, import.meta.env.VITE_BASE_URL)
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     return savedPosition || { left: 0, top: 0 }
